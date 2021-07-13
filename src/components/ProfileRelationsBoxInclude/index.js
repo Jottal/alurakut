86624790fa01a2408a019023fe475cf4b6800a16
Joao Lucca Oliveira Schmitz <joao.schmitz@cwi.com.br>
2021-07-13 08:54:01 -0300

    fix bug

--- src/components/ProfileRelationsBoxInclude/index.js
+++ src/components/ProfileRelationsBoxInclude/index.js
@@ -1,26 +1,30 @@
 import { ProfileRelationsBoxWrapper } from "../../components";
 
 export const ProfileRelationsBoxInclude = ({ title, list }) => {
+  const runCards = () => {
+    const newList = list.slice(0, 5);
+
+    return newList.map((itemAtual) => {
+      return (
+        <li key={itemAtual.id}>
+          <a href={itemAtual.direct}>
+            <img src={itemAtual.image} />
+            <span>{itemAtual.title}</span>
+          </a>
+        </li>
+      );
+    });
+  };
+
   return (
     <ProfileRelationsBoxWrapper>
       <h2 className="smallTitle">
         {title} ({list.length})
       </h2>
 
-      <ul>
-        {list.map((itemAtual) => {
-          return (
-            <li key={itemAtual.id}>
-              <a href={itemAtual.direct}>
-                <img src={itemAtual.image} />
-                <span>{itemAtual.title}</span>
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      <ul>{runCards()}</ul>
 
-      {list.length > 5 && <a className="smallTitle">Ver todos</a>}
+      {list.length > 6 && <a className="smallTitle">Ver todos</a>}
     </ProfileRelationsBoxWrapper>
   );
 };
